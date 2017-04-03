b49d548dd9e65cf69c3e35d18e41dd40c60632a6
Wasid Hossain <wasidmail@gmail.com>
2017-04-03 04:43:10 +0000

    basic function added with jQuery

--- assets/js/lib/main.js
+++ assets/js/lib/main.js
@@ -1,3 +1,25 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    $("li").on("click", function(){
+        
+        // console.log($(this).css("color"));
+        
+        if ($(this).css("color") === "rgb(128, 128, 128)") {
+            
+            $(this).css({
+            color: "#28292B",  
+            textDecoration: "none"
+        });
+            
+        } else {
+            
+                    
+        $(this).css({
+            color: "gray",  
+            textDecoration: "line-through"
+        });
+            
+        }
+  
+    });
+    
 });
